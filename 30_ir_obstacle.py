c99a3c8188fc0ac7756c1520e8f977d347058339
kajoban <kuhapark@mcmaster.ca>
2018-01-28 01:13:30 -0500

    Reverted to original

--- 30_ir_obstacle.py
+++ 30_ir_obstacle.py
@@ -8,21 +8,11 @@ def setup():
 	GPIO.setup(ObstaclePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def loop():
-        
-        status = 1; #clear 
-        
 	while True:
-                
-		if (0 == GPIO.input(ObstaclePin)  and status == 1):  #while having no object is false
+		if (0 == GPIO.input(ObstaclePin)):  #while having no object is false
 			print "Status: Obstruction Detected!"
-			status = 0; #now obstructed 
-			
-		else if (0 != GPIO.input(ObstaclePin) and status == 0):  #while having no object is false
-			print "Status: Clear."
-			status = 1; #now clear 
-        
-
-                #status is either clear, or an obstruction has been detected. 
+		else:
+                        print "Status: Clear."
 			
 
 def destroy():
@@ -34,4 +24,3 @@ if __name__ == '__main__':     # Program start from here
 		loop()
 	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
 		destroy()
-
