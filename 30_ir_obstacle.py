9041075ec4e66da8278781dba58b1e13ba020aab
kajoban <kuhapark@mcmaster.ca>
2018-01-28 01:27:23 -0500

    Reduce spam

--- 30_ir_obstacle.py
+++ 30_ir_obstacle.py
@@ -12,12 +12,13 @@ def loop():
         status = 1 #status is clear initially
         
 	while True:
-		if (0 == GPIO.input(ObstaclePin) and status == 1):  #while having no object is false and status is clear
-			print "Status: Obstruction Detected!"
-			status = 0 #status changes to obstructed 
-		elif (0 != GPIO.input(ObstaclePin) and status == 0): #while having no object is true and status is obstructed 
-                        print "Status: Clear."
-                        status = 1 #status changes to clear 
+                
+		if (GPIO.input(ObstaclePin) and status == 0):  #while having no object is false and status is clear
+			print "Status: Clear."
+			status = 1 #status changes to obstructed 
+		elif ( !(GPIO.input(ObstaclePin)) and status == 1): #while having no object is true and status is obstructed 
+                        print "Status: Obstruction Detected!"
+                        status = 0 #status changes to clear 
 			
 
 def destroy():
